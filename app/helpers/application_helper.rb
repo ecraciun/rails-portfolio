89069b674363d264a3ea1c37ad4e77f319a58ed5
Emil Craciun <emil.craciun@live.com>
2021-09-01 15:32:09 +0300

    More helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,23 @@ module ApplicationHelper
     '<p>My helper</p>'.html_safe
   end
 
+  def another_sample_helper
+    content_tag(:div, 'My content', class: 'my-class')
+  end
+
   def login_helper
     if current_user.is_a?(User)
       link_to 'Logout', destroy_user_session_path, method: :delete
     else
       (link_to 'Login', new_user_session_path) + ' | ' +
         (link_to 'Register', new_user_registration_path)
+    end
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      content_tag(:p, "Thanks for visiting me from #{session[:source]} and you are on #{layout_name}",
+                  class: 'source-greeting')
     end
   end
 end
