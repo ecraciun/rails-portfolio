2d7cb54f4fb9db4889756a52ee2c6dbf2850c162
Emil Craciun <emil.craciun@live.com>
2021-09-02 16:07:09 +0300

    Copyright "gem"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = MiliCopyViewTool::Renderer.copyright 'Emil C', 'All rights reserved'
+  end
+end
+
+# Yeah this was supposed to be in a custom built gem, but I'm not going to do that. Got how it works
+module MiliCopyViewTool
+  class Renderer
+    def self.copyright(name, message)
+      "&copy #{Time.now.year} | <b>#{name}</b> #{message}".html_safe
+    end
+  end
 end
