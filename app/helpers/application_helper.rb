e1a650bb794286fd31d8af1ac72f5bf952d237c7
Emil Craciun <emil.craciun@live.com>
2021-09-08 15:09:09 +0300

    Refactored navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,57 @@ module ApplicationHelper
                   class: 'source-greeting')
     end
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+      {
+        url: about_path,
+        title: 'About'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: '/rails/info/routes',
+        title: 'ROUTES'
+      }
+    ]
+  end
+
+  def nav_helper(style, tag_type)
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def nav_helper_heredocs(style, tag_type)
+    nav_links = <<~NAV_STRING
+      <#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+      <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blogs</a></#{tag_type}>
+      <#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
+      <#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
+      <#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+      <#{tag_type}><a href="/rails/info/routes" class="#{style} #{active? '/rails/info/routes'}">ROUTES</a></#{tag_type}>
+    NAV_STRING
+    nav_links.html_safe
+  end
+
+  def active?(path)
+    'active' if current_page? path
+  end
 end
