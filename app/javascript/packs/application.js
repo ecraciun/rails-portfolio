4189b2beba3707e215113db2815e69ada8000917
Emil Craciun <emil.craciun@live.com>
2021-09-13 15:11:55 +0300

    JS libraries work, added sortable to portfolios

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,19 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require("packs/html.sortable")
+require("packs/portfolios")
+
+import $ from 'jquery';
+
+global.$ = $
+global.jQuery = $
+
+
+require('jquery-ui');
+
+// jquery-ui theme
+require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.css/ );
+require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.theme\.css/ );
 
 Rails.start()
 Turbolinks.start()
