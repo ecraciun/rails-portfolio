50fe15014ef04f5818d4179d4eccac356c8f9d1c
Emil Craciun <emil.craciun@live.com>
2021-09-23 13:23:20 +0300

    Updated form and refactored placeholder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,53 +7,49 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 3.times do |topic|
-    Topic.create!(
-        title: "Topic #{topic + 1}"
-    )
+  Topic.create!(
+    title: "Topic #{topic + 1}"
+  )
 end
 
-puts "3 topics created"
-
+puts '3 topics created'
 
 10.times do |blog|
-    Blog.create!(
-        title: "My Blog Post #{blog + 1}",
-        body: "This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.",
-        topic_id: Topic.last.id
-    )
+  Blog.create!(
+    title: "My Blog Post #{blog + 1}",
+    body: 'This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.This file should contain all the record creation needed to seed the database with its default values.',
+    topic_id: Topic.last.id
+  )
 end
 
-puts "10 blog posts created"
-
+puts '10 blog posts created'
 
 5.times do |skill|
-    Skill.create!(
-        title: "Rails #{skill + 1}",
-        percent_utilized: 15
-    )
+  Skill.create!(
+    title: "Rails #{skill + 1}",
+    percent_utilized: 15
+  )
 end
 
-puts "5 skills created"
-
+puts '5 skills created'
 
 9.times do |portfolio|
-    Portfolio.create!(
-        title: "Portfolio title #{portfolio + 1}",
-        subtitle: "Etc bla bla bla",
-        body: "Some body. The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).",
-        main_image: "http://placehold.it/600x400",
-        thumb_image: "http://placehold.it/350x200"
-    )
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio + 1}",
+    subtitle: 'Etc bla bla bla',
+    body: 'Some body. The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).',
+    main_image: 'http://placehold.it/600x400',
+    thumb_image: 'http://placehold.it/350x200'
+  )
 end
 
-puts "9 portfolio items created"
-
+puts '9 portfolio items created'
 
 3.times do |technology|
-    Technology.create!(
-        title: "Technology #{technology + 1}"
-        portfolio_id: Portfolio.last.id
-    )
+  Technology.create!(
+    name: "Technology #{technology + 1}",
+    portfolio_id: Portfolio.last.id
+  )
 end
 
-puts "3 technologies created"
+puts '3 technologies created'
